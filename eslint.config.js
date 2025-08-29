31ec2dfc0160c579c13e89f73f116f843a07f921
caique <caiquealves224@gmail.com>
2025-08-29 18:40:21 -0300

    feat: initial commit with config files

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      // Regras básicas do ESLint
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      
+      // Integração com Prettier
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: [
+      'dist/',
+      'node_modules/',
+      '*.config.js',
+      '*.config.ts',
+    ],
+  }
+];
